26a0957073d55ba8e66a156a05d4ed2f6f81329f
Shy <shyguyx1412@gmail.com>
2025-12-18 02:22:04 +0100

    load and run tests

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,25 @@ use bus::*;
 use ic6502::IC6502;
 use test::TestCase;
 
-const ACCURACY_COIN: &[u8] = include_bytes!("../AccuracyCoin/AccuracyCoin.nes");
+const CASES_RAW: &str = include_str!("../65x02/rockwell65c02/v1/00.json");
 
 fn main() {
     let ram = Box::new([0; u16::MAX as usize]);
     let cpu = IC6502::default();
+
+    let test_cases: Vec<TestCase<IC6502>> = match serde_json::from_str(CASES_RAW) {
+        Ok(val) => val,
+        Err(err) => panic!("{}", err),
+    };
+
+    for case in test_cases.iter() {
+        let mut cpu = case.initial.cpu.clone();
+        let mut ram = case.initial.ram.clone();
+        let cycles = case.cycles.len();
+        for _ in 0..cycles {
+            cpu.cycle(&mut ram);
+        }
+        break;
+    }
     println!("Hello, world!");
 }
--- src/test.rs
+++ src/test.rs
@@ -1,9 +1,39 @@
-struct TestCase {
-  name:String,
-  initial: crate::IC6502,
+use serde_derive::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TestCase<T> {
+    name: String,
+    pub initial: State<T>,
+    #[serde(rename = "final")]
+    pub target: State<T>,
+    pub cycles: Vec<(u16, u8, String)>,
 }
 
-struct State {
-  
+#[derive(Debug, Serialize, Deserialize)]
+pub struct State<T> {
+    #[serde(flatten)]
+    pub cpu: T,
+    pub ram: Vec<(u16, u8)>,
+}
+
+impl crate::bus::OpenBus for Vec<(u16, u8)> {
+    fn read(&self, addr: u16) -> Option<u8> {
+        self.iter()
+            .find_map(|(ram_addr, val)| match *ram_addr == addr {
+                true => Some(*val),
+                false => None,
+            })
+    }
+
+    fn write(&mut self, addr: u16, byte: u8) {
+        let value = (addr, byte);
+        for (ram_addr, val) in self.iter_mut() {
+            if (*ram_addr != addr) {
+                continue;
+            }
+            *val = byte;
+            return;
+        }
+        self.push(value);
+    }
 }
